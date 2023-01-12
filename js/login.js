707614a61df042d4097ca0a262709c8a005ab0f1
Joaquin <joaquinutnfrt@gmail.com>
2023-01-12 08:15:55 -0300

    adding javascript code to the login page

--- js/login.js
+++ js/login.js
@@ -1,8 +1,8 @@
 (function () {
   'use strict'
-
-  // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
   var forms = document.querySelectorAll('.needs-validation')
+  
+  // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
 
   // Bucle sobre ellos y evitar el envío
   Array.prototype.slice.call(forms)
@@ -11,9 +11,34 @@
         if (!form.checkValidity()) {
           event.preventDefault()
           event.stopPropagation()
+          login()
         }
-
+        // else login()
         form.classList.add('was-validated')
+        
       }, false)
     })
 })()
+
+const getUsers = async() => {
+  return (
+    await fetch('http://localhost:3000/users')
+            .then(res => res.json())
+            .then(users => users)
+        )
+}
+
+const saveUser = user => localStorage.setItem('user', JSON.stringify(user))
+
+const login = async() => {
+  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value
+  const users = await getUsers()
+
+  const user = users.find(user => user.emil === email && user.password === email.password)
+
+  if(user === undefined) return alert('EL USUARIO QUE DESEA INGRESAR NO ESTA REGISTRADO')
+  saveUser(user)
+  window.open(window.open("../pages/index.html", "_self"))
+
+}
